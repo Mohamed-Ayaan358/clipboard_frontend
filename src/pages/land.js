1c67f3ee0d13a3cb7956ab99d27a052fd931275a
Mohamed-Ayaan358 <ayaan35813@gmail.com>
2021-11-02 17:56:50 +0530

    Added .css files

--- src/pages/land.js
+++ src/pages/land.js
@@ -25,64 +25,42 @@ function Land() {
         </h4>
       </div>
 
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {/* <DrawerHeader /> */}
-        <Typography
-          paragraph
-          style={{
-            fontWeight: "bold",
-            fontSize: "40px",
-            fontFamily: "Roboto",
-            textAlign: "center",
-            color: "#E42346",
-          }}
-        >
-          Compile everything.
-        </Typography>
-        <Typography
-          paragraph
-          style={{
-            fontWeight: "bold",
-            fontSize: "20px",
-            fontFamily: "Roboto",
-            textAlign: "center",
-            color: "#E42346",
-          }}
-        >
-          Even if its a simple note
-        </Typography>
-        <Typography
-          paragraph
-          style={{
-            fontSize: "20px",
-            fontFamily: "Roboto",
-            textAlign: "center",
-            color: "#E42346",
-          }}
-        >
-          What is PinBoard you ask? It’s a hyper effective planner for your
-          daily needs.
-          <br />
-          What are you waiting for, get registered now!
-        </Typography>
-        <div style={{ paddingTop: "100px", textAlign: "center" }}>
-          <Link to="/home">
-            <button
-              style={{
-                fontSize: "20px",
-                borderRadius: "10px",
-                height: "50px",
-                width: "300px",
-                backgroundColor: "#0D0C1D",
-                Left: "200px",
-                color: "#FFFFFF",
-              }}
-            >
-              Let's get started
-            </button>
-          </Link>
-        </div>
-      </Box>
+    const returnmain = () => {
+        ReactDOM.render(<App />, document.getElementById('root'));
+    }
+
+    return (
+        <>
+            <div class="img-with-text">
+                <img id="logo1" src={Pin} />
+                <h4 id="caption1">Clipboard</h4>
+            </div>
+
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }} >
+                {/* <DrawerHeader /> */}
+                < Typography paragraph id="tagline1">
+                    Compile everything.
+
+                </Typography>
+                <Typography paragraph id="tagline2" >
+
+                    Even if its a simple note
+                </Typography>
+                <Typography paragraph id="tagline3" >
+                    What is PinBoard you ask?
+                    It’s a hyper effective planner for your daily needs.
+                    <br />What are you waiting for, get registered now!
+                </Typography>
+                <div style={{ paddingTop: "100px", textAlign: "center" }}>
+                    <button id="getstarted" onClick={returnmain} >Let's get started</button>
+                </div>
+            </Box>
+
+            {/* Need to modify the contents of the actual navbar*/}
+            <div id="bottom-bar" class="navbar" >
+                <p id="about-us" > About us &emsp;</p>
+
+            </div >
 
       {/* Need to modify the contents of the actual navbar*/}
       <div
