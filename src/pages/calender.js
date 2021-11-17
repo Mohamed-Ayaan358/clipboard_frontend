ed38c803a9d097d84d7ae6dec57ea82551544f07
NavinShrinivas <karupal2002@gmail.com>
2021-11-17 23:07:46 +0530

    Basic and nice calnder comp
    Signed-off-by: NavinShrinivas <karupal2002@gmail.com>
    
    This component is now prime to add databse shit , imma start
    that tmrw

--- src/pages/calender.js
+++ src/pages/calender.js
@@ -4,20 +4,46 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import moment from "moment";
 import "./calender.css";
-
+/*
+ *  <p>
+ *          Current selected date is{" "}
+ *          <b>{moment(dateState).format("MMMM Do YYYY")}</b>
+ *      </p>
+ *
+ */
 function CalendarComp() {
   const [dateState, setDateState] = useState(new Date());
   const changeDate = (e) => {
     setDateState(e);
   };
+  const [quote, setQuote] = useState(" ");
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("https://api.quotable.io/random");
+      const json = await result.json();
+      console.log(json);
+      setQuote(json);
+    };
+    fetchData();
+  }, []);
   return (
-    <div>
-      <Calendar value={dateState} onChange={changeDate} />
-      <p>
-        Current selected date is{" "}
-        <b>{moment(dateState).format("MMMM Do YYYY")}</b>
-      </p>
-    </div>
+    <Box class="mainbox">
+      <h1>Calender</h1>
+      <div class="sleepingdiv">
+        <Calendar value={dateState} onChange={changeDate} />
+        <p class="tolist">
+          <h2>To-Do {moment(dateState).format("MMMM Do YYYY")}</h2>
+          <ul>
+            <li>hello</li>
+          </ul>
+        </p>
+      </div>
+      <div class="dailyquote">
+        <h2>Quote of the day</h2>
+        <p class="linebreak">{quote.content}</p>
+        <p>- {quote.author}</p>
+      </div>
+    </Box>
   );
 }
 export default CalendarComp;
