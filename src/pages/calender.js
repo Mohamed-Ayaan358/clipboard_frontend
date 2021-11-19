6af199e5d270b1f94d22eba9793c8ad378bc97e3
NavinShrinivas <karupal2002@gmail.com>
2021-11-19 23:24:36 +0530

    Finally shit looks half decent :))
    Signed-off-by: NavinShrinivas <karupal2002@gmail.com>
    
    MUI Docs for the win!!!

--- src/pages/calender.js
+++ src/pages/calender.js
@@ -4,6 +4,11 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import moment from "moment";
 import "./calender.css";
+
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { FixedSizeList } from "react-window";
 /*
  *  <p>
  *          Current selected date is{" "}
@@ -11,6 +16,18 @@ import "./calender.css";
  *      </p>
  *
  */
+
+function renderRow(props) {
+  const { index, style } = props;
+
+  return (
+    <ListItem style={style} key={index} component="div" disablePadding>
+      <ListItemButton>
+        <ListItemText primary={`Item ${index + 1}`} />
+      </ListItemButton>
+    </ListItem>
+  );
+}
 function CalendarComp() {
   const [dateState, setDateState] = useState(new Date());
   const changeDate = (e) => {
@@ -32,17 +49,26 @@ function CalendarComp() {
       <div class="standingdiv">
         <div class="sleepingdiv">
           <Calendar value={dateState} onChange={changeDate} />
-          <p class="tolist">
-            <h2>To-Do {moment(dateState).format("MMMM Do YYYY")}</h2>
-            <ul class="list">
-              <li class="bottom">Add Work :(</li>
-            </ul>
-          </p>
-        </div>
-        <div class="dailyquote">
-          <h2>Quote of the day</h2>
-          <p class="linebreak">{quote.quote}</p>
-          <p>- {quote.author}</p>
+          <div class="compose">
+            <div class="listdiv">
+              <h2>To-Do {moment(dateState).format("MMMM Do YYYY")}</h2>
+              <div class="list">
+                <FixedSizeList
+                  height={400}
+                  itemSize={46}
+                  itemCount={200}
+                  overscanCount={5}
+                >
+                  {renderRow}
+                </FixedSizeList>
+              </div>
+            </div>
+            <div class="dailyquote">
+              <h4>Quote of the day</h4>
+              <p class="linebreak">{quote.quote}</p>
+              <p>- {quote.author}</p>
+            </div>
+          </div>
         </div>
       </div>
     </Box>
