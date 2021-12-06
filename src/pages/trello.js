9be95de016389786186fa31b0ce9647a3e47ffe2
Mohamed-Ayaan358 <ayaan35813@gmail.com>
2021-12-06 21:50:04 +0530

    Altered trello.css

--- src/pages/trello.js
+++ src/pages/trello.js
@@ -16,14 +16,14 @@ function Trello() {
             backgroundColor: "white",
             width: "280px",
             borderRadius: "15px",
-            border: "2px solid red",
+            border: "2px solid #E42346",
           }}
           laneStyle={{
             backgroundColor: "#0D0C1D",
-            color: "red",
-            borderRadius: "10px",
+            color: "#E42346",
+            borderRadius: "20px",
           }}
-          style={{ backgroundColor: "white", color: "red" }}
+          style={{ backgroundColor: "white", color: "#E42346" }}
           editLaneTitle
           editable
           draggable
